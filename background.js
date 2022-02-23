c25478ab438d2b85e785fe38e579e96064f66e58
vinhero <v-uber@web.de>
2022-02-24 00:06:18 +0100

    yes

--- background.js
+++ background.js
@@ -1,39 +1,7 @@
-// require
-const fetch = require("node-fetch");
+var apiurl = 'https://api.kyroskoh.xyz/valorant/v1/mmr/eu/';
+var params = '?show=rr&rank&display=0'
 
-// Blocks of URL that is scraped
-// website + area + [player] + settings
-const website = "https://tracker.gg/valorant/"
-const area = "profile/riot/"
-const filtersettings = "/overview?playlist=competitive&season=all"
-
-// function to get the raw data
-const getRawData = (URL) => {
-   return fetch(URL)
-      .then((response) => response.text())
-      .then((data) => {
-         return data;
-      });
-};
-
-// start of the program
-const getCricketWorldCupsList = async () => {
-   const cricketWorldCupRawData = await getRawData(URL);
-   console.log(cricketWorldCupRawData);
-};
-
-function buildPlayer(strPlayerName, strPlayerTag){
-    
-    return strPlayerName.replace(' ', '%20') + strPlayerTag.replace('#', '%23');
-}
-
-function getPlayerRank(strPlayerName, strPlayerTag){
-    var playerurl = website + area + buildPlayer(strPlayerName, strPlayerTag) + filtersettings;
-    console.log(playerurl);
-    
-    const playersite = getRawData(playerurl);
-    console.log(playersite);
-}
-
-// invoking the main function
-getPlayerRank("vinhero", "#GAIN");
+chrome.runtime.onInstalled.addListener(() => {
+   chrome.storage.sync.set({ apiurl });
+   chrome.storage.sync.set({ params });
+ });
