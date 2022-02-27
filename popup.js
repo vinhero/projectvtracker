988082f317b5684114aca62037b7efc73e764a87
vinhero <v-uber@web.de>
2022-02-28 00:39:19 +0100

    getting rid of chrome.storage

--- popup.js
+++ popup.js
@@ -10,62 +10,70 @@ m_btnGetRanks.addEventListener("click", async () => {
 });
 
 function getAllRanks() {
-    const arrHtmlPlayer = document.getElementsByClassName('match-overview__member-gameaccount');
-    let arrPlayerRIDs = [];
     
-    function getPlayerRank(arrPlayerIDs){
+    // -------------------------- Variables -------------------------- //
+    const arrHtmlPlayer = document.getElementsByClassName('match-overview__member-gameaccount');
+    
+    let arrPlayerRIDs = [];
+    // let strApiUrl = 'https://api.kyroskoh.xyz/valorant/v1/mmr/eu/';
+    // let strParameters = '?show=rr&rank&display=0'
+
+    let strApiUrl = 'https://api.kyroskoh.xyz/valorant/v1/mmr/eu/';
+    let strParameters = '?show=rr&rank&display=0'
+    
+
+    // -------------------------- Functions -------------------------- //
+    function getRiotID (strUnshortened) {
+        let strShortened = null;
+        strShortened = strUnshortened.replace(/[\r\n]/gm, '');
+        strShortened = strShortened.trimStart();
+        strShortened = strShortened.trimEnd();
+        return strShortened;
+    }
+    
+    function getPlayerRanks(arrPlayerIDs){
+        let arrPlayerURLs = [];
         arrPlayerIDs.forEach(strPlayerID => {
-            buildPlayerURL(strPlayerID);
+            arrPlayerURLs = buildPlayerURL(strPlayerID, arrPlayerURLs);
+            
+            let strSideText = null;
+            fetch(arrPlayerURLs[arrPlayerURLs.length-1], {
+                mode: 'cors'
+            }).then((data) => {
+                data = strSideText;
+                console.log(data);
+            });            
         });
-
-        chrome.storage.sync.get(['arrPlayerURLs'], (arrData) => {
-            arrData.arrPlayerURLs.forEach(strPlayerURL => {
-                let strSideText = null;
-                fetch(strPlayerURL, {
-                    mode: 'no-cors'
-                }).then((data) => {
-                    data = strSideText;
-                });
-                console.log(strSideText);
-            });
-        });
-
     }
 
-    function buildPlayerURL(strPlayerID){        
+    function buildPlayerURL(strPlayerID, arrPlayerURLs){        
         let nIndexHashtag = strPlayerID.indexOf('#');
-        let strPlayerName = ''; 
+        let strPlayerName = '';
         let strPlayerTag = '';
-        for (let nCharIndex = 0; strPlayerID.length > nCharIndex; nCharIndex++) 
+        for (let nCharIndex = 0; strPlayerID.length > nCharIndex; nCharIndex++)
         {
             if (nCharIndex < nIndexHashtag)
             {
                 strPlayerName += strPlayerID[nCharIndex];
-            } 
+            }
             else if (nCharIndex > nIndexHashtag)
             {
                 strPlayerTag += strPlayerID[nCharIndex];
             }
             else {/** Do not add the character anywhere. */}
         }
-        chrome.storage.sync.get(['strApiUrl', 'strParameters', 'arrPlayerURLs'], (arrData) => {
-            arrData.arrPlayerURLs[arrData.arrPlayerURLs.length] = (arrData.strApiUrl + strPlayerName + '/' + strPlayerTag + arrData.strParameters).replaceAll(' ', '%20');
-            //console.log(arrData.arrPlayerURLs[arrData.arrPlayerURLs.length-1]);
-            chrome.storage.sync.set({arrPlayerURLs: arrData.arrPlayerURLs});
-        });
-    }
-    
-    for (let index = 0; arrHtmlPlayer.length > index; index++) 
-    {
-        let strRiotID = arrHtmlPlayer[index].textContent;
-        strRiotID = strRiotID.replace(/[\r\n]/gm, '');
-        strRiotID = strRiotID.trimStart();
-        strRiotID = strRiotID.trimEnd();
-        arrPlayerRIDs[index] = strRiotID;
-        //console.log(strRiotID);
+        arrPlayerURLs[arrPlayerURLs.length] = (strApiUrl + strPlayerName + '/' + strPlayerTag + strParameters).replaceAll(' ', '%20');
+        return arrPlayerURLs;
     }
 
-    chrome.storage.sync.set({arrPlayers: arrPlayerRIDs}, () => {
-        getPlayerRank(arrPlayerRIDs);
-    });
+    // -------------------------- Start -------------------------- //
+    // creates all RiotIDs from the MatchSide
+    for (let index = 0; arrHtmlPlayer.length > index; index++) 
+    {
+        let strRiotID = getRiotID(arrHtmlPlayer[index].textContent);
+        arrPlayerRIDs[index] = strRiotID;
+        
+        //console.log(strRiotID);
+    }
+    getPlayerRanks(arrPlayerRIDs);
 }
