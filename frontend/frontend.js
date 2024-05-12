f23571613230dc7bd5f531d9fc6d8af89ea0fc51
Vincent Uber <v-uber@web.de>
2024-05-12 22:57:14 +0200

    faster frontend

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -44,31 +44,25 @@ async function enhanceMatch() {
     console.log("enhancing Match");
 
     let arrPlayers = [];
-    let strStatus = "";
+    // let strStatus = "";
     chrome.runtime.sendMessage({type: "enhanceMatch", matchID: document.baseURI.replace(strMatchUrl, "")}, function(response) {
         if (response.success) {
-            console.log("success");
             let jsonData = response.data;
-            console.log(jsonData);
-            strStatus = jsonData.status;
+            // strStatus = jsonData.status;
             for (let lineup in jsonData.encounters){
                 for (let player in jsonData.encounters[lineup].lineups){
                     arrPlayers.push(jsonData.encounters[lineup].lineups[player].user.gameaccounts[0].value);
                 }
             }
+
+            getPlayerInfos(arrPlayers)
+            .then(response => {
+                addRanksToMatchpage(response);
+            });
         } else {
           console.error('Fehler beim Abrufen der Daten:', response.error);
         }
     });
-    
-
-    let dictMatchInfo = { };
-
-    let blnTeamAreReady = strStatus != "PENDING";
-    dictMatchInfo = await createMatchInfo(htmlCollection_Teams, !blnTeamAreReady);
-    console.log(dictMatchInfo);
-
-    addRanksToMatchpage(dictMatchInfo);
 }
 
 async function enhanceTeam() {
@@ -83,14 +77,6 @@ async function enhanceTeam() {
 
     // Replace Logo with Rank
     document.querySelector("." + strRankClassName).replaceWith(htmlRankElement);
-}
-
-function getRiotID (strUnshortened) {
-    let strShortened = null;
-    strShortened = strUnshortened.replace(/[\r\n]/gm, '');
-    strShortened = strShortened.trimStart();
-    strShortened = strShortened.trimEnd();
-    return strShortened;
 }
 
 function buildApiUrl(strPlayerID) {        
@@ -114,69 +100,8 @@ function buildApiUrl(strPlayerID) {
     return (strApiUrl + strPlayerName + '/' + strPlayerTag).replaceAll(' ', '%20');
 }
 
-async function createMatchInfo(htmlCollection) {
-    return await createMatchInfo(htmlCollection, false);
-}
-
-async function createMatchInfo(htmlCollection, blnScraping) {
-    let dictReturn = { };
-    
-    // build Teams
-    for (let nTeamIndex = 0; nTeamIndex < htmlCollection.length; nTeamIndex++) {
-        let dictTeam = { };
-        let strSide = nTeamIndex == 0 ? "Left" : nTeamIndex == 1 ? "Right" : null;
-        let team = htmlCollection[nTeamIndex].children;
-        
-        // build Players
-        let arrRiotIDs = [];
-        let arrProfileNames = [];
-        for (let nPlayerIndex = 0; nPlayerIndex < team.length; nPlayerIndex++) {
-            const strPlayerClassName = "match-overview__member";
-            const strProfileNameClassName = "match-overview__member-username";
-            const strPlayerIDClassName = "match-overview__member-gameaccount";
-            let htmlPlayer = team[nPlayerIndex];
-            
-            // Element is a Player
-            if (htmlPlayer.className == strPlayerClassName) {
-                let strProfileName = htmlPlayer.querySelector("." + strProfileNameClassName).title;
-                arrProfileNames.push(strProfileName);
-                let strRiotID = "";
-                
-                // RiotIDs are on the Matchpage
-                if (blnScraping === false || blnScraping == null){
-                    strRiotID = getRiotID(htmlPlayer.querySelector("." + strPlayerIDClassName).textContent);
-                }
-                    
-                // RiotIDs have to be Scraped from the Players Profilepage
-                else if (blnScraping === true){
-                    let strScrapedRiotID = scrapeRiotID(strProfileName);
-                    strRiotID = getRiotID(strScrapedRiotID);
-                }
-                
-                else { /** Case not specified yet. */ }
-                arrRiotIDs.push(strRiotID);
-            }
-            else { /** Not a Player. */ }
-        }
-        console.log(arrRiotIDs);
-        await getPlayerInfos(arrRiotIDs, arrProfileNames)
-        
-        // add team to matchinfo
-        .then((players) => {
-            dictTeam.Players = players;
-            dictReturn[strSide] = dictTeam;
-        });
-    }
-
-    return dictReturn;
-}
-
 // Promises
-async function getPlayerInfos(arrRiotIDs){
-    return await getPlayerInfos(arrRiotIDs, null);
-}
-
-async function getPlayerInfos(arrRiotIDs, arrProfileNames) {
+async function getPlayerInfos(arrRiotIDs) {
     const strUnrankedUrl = "https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/0.png";
     
     let arrPlayerInfos = [];
@@ -186,11 +111,6 @@ async function getPlayerInfos(arrRiotIDs, arrProfileNames) {
         let strApiUrl = buildApiUrl(strRiotID);
         
         let objPlayerInfo = new Object();
-        if (arrProfileNames != null) {
-            objPlayerInfo.Profile = arrProfileNames[nIdIndex];
-        }
-        else { /** no profile names givin. */}
-
         objPlayerInfo.RiotID = strRiotID;
         arrPromises.push(fetch(strApiUrl)
         
@@ -223,31 +143,16 @@ async function getPlayerInfos(arrRiotIDs, arrProfileNames) {
     return arrPlayerInfos;
 }
 
-function scrapeRiotID(strProfileName) {
-    let strRiotID = "";
+function addRanksToMatchpage(dictPlayerInfos) {
+    for (let player of dictPlayerInfos) {
+        let rankFactory = new RankFactory();
+        let htmlRankElement = rankFactory.createMatchElement(player);
 
-    // convert profilename, so it can be used in the url
-    let strConvertedProfileName = strProfileName.toLowerCase();
-    strConvertedProfileName = strConvertedProfileName.replaceAll(" ", "-");
-
-    let strScrapeUrl = strProfileUrl + strConvertedProfileName;
-
-    return strRiotID;
-}
-
-function addRanksToMatchpage(dictTeamInfos) {
-    for (let side in dictTeamInfos) {
-        let team = dictTeamInfos[side];
-        for (let player of team.Players) {
-            let rankFactory = new RankFactory();
-            let htmlRankElement = rankFactory.createMatchElement(player);
-
-            for (let element = 0; element < document.getElementsByClassName("match-overview__member").length; element++){
-                if (document.getElementsByClassName("match-overview__member")[element].getElementsByClassName("match-overview__member-gameaccount")[0].textContent.includes(player.RiotID)) {
-                    document.getElementsByClassName("match-overview__member")[element].appendChild(htmlRankElement);
-                }
+        for (let element = 0; element < document.getElementsByClassName("match-overview__member").length; element++){
+            if (document.getElementsByClassName("match-overview__member")[element].getElementsByClassName("match-overview__member-gameaccount")[0].textContent.includes(player.RiotID)) {
+                document.getElementsByClassName("match-overview__member")[element].appendChild(htmlRankElement);
             }
         }
-        document.getElementsByClassName("match-overview__member")[0].getElementsByClassName("match-overview__member-gameaccount")[0].textContent
     }
+    document.getElementsByClassName("match-overview__member")[0].getElementsByClassName("match-overview__member-gameaccount")[0].textContent
 }
