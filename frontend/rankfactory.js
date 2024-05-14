5d85e0a501ff478969f34bbb52b03c3bcb36cdf1
Vincent Uber <v-uber@web.de>
2024-05-15 00:41:17 +0200

    rankElement adjusted

--- frontend/rankfactory.js
+++ frontend/rankfactory.js
@@ -1,11 +1,11 @@
 class RankFactory {
-    constructor(strTrackerUrl, strTrackerSection) {
+    constructor() {
         this.strWidth = "90";
         this.strHeight = "90";
         this.strPaddingButtom = "10";
         this.strTagName = "IMG";
-        this.strTrackerUrl = strTrackerUrl;
-        this.strTrackerSection = strTrackerSection;
+        this.strTrackerUrl = "https://tracker.gg/valorant/profile/riot/";
+        this.strTrackerSection = "/overview";
     }
 
     setupPlayerElement() {
@@ -16,22 +16,34 @@ class RankFactory {
     }
 
     setupMatchElement() {
-        this.strWidth = "40";
-        this.strHeight = "40";
+        this.strActualHeight = "40";
+        this.strActualWidth = "40";
+        this.strWidth = "20px";
+        this.strHeight = "20px";
+        this.strMargin = "10px";
+        this.strMarginTop = "42px";
         this.strPaddingButtom = "15";
         this.strTagName = "IMG";
+        this.strPosition = "absolute";
     }
     
     createHtmlRank(player) {
         let strOnclick = `window.open(\'${this.strTrackerUrl}${player.RiotID.replaceAll('#', '%23')}/${this.strTrackerSection}\')`;
         let htmlRankElement = document.createElement(this.strTagName);
         
-        htmlRankElement.src = player.RankImg;
-        htmlRankElement.width = this.strWidth;
-        htmlRankElement.height = this.strHeight;
-        htmlRankElement.padding = this.strPaddingButtom;
-        htmlRankElement.onClick = strOnclick;
+        // Set Attributes
+        htmlRankElement.width = this.strActualWidth;
+        htmlRankElement.height = this.strActualHeight;
+        htmlRankElement.style.width = this.strWidth; 
+        htmlRankElement.style.height = this.strHeight;
+        htmlRankElement.style.margin = this.strMargin;
+        htmlRankElement.style.marginTop = this.strMarginTop;
+        htmlRankElement.style.paddingBottom = this.strPaddingButtom;
+        htmlRankElement.style.position = this.strPosition;
         htmlRankElement.style.zIndex = "1000";
+        
+        htmlRankElement.src = player.RankImg;
+        htmlRankElement.onClick = strOnclick;
         
         return htmlRankElement;
     }
