7739f12634287c1cba0ce10485c31a0c40baa4b0
Vincent Uber <v-uber@web.de>
2024-05-23 21:19:00 +0200

    alt api added

--- backend/backend.js
+++ backend/backend.js
@@ -7,6 +7,33 @@ const strUnrankedUrl = "https://trackercdn.com/cdn/tracker.gg/valorant/icons/tie
 const strProjectVApiUrl = "https://api.projectv.gg/api/v1/frontend/matches/";
 const strProjectVApiKeys = "?expand=encounters.lineups.user.gameaccounts";
 
+const dictRankIMG = 
+    {
+        'Unranked': "https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/0.png",
+        'Iron 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/3.png',
+        'Iron 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/4.png',
+        'Iron 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/5.png',
+        'Bronze 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/6.png',
+        'Bronze 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/7.png',
+        'Bronze 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/8.png',
+        'Silver 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/9.png',
+        'Silver 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/10.png',
+        'Silver 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/11.png',
+        'Gold 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/12.png',
+        'Gold 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/13.png',
+        'Gold 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/14.png',
+        'Platin 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/15.png',
+        'Platin 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/16.png',
+        'Platin 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/17.png',
+        'Diamond 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/18.png',
+        'Diamond 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/19.png',
+        'Diamond 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/20.png',
+        'Immortal 1': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/21.png',
+        'Immortal 2': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/22.png',
+        'Immortal 3': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/23.png',
+        'Radiant': 'https://trackercdn.com/cdn/tracker.gg/valorant/icons/tiers/24.png',
+    }
+
 const strProfileUrl = "https://projectv.gg/profile/";
 const strMatchUrl = "https://projectv.gg/matches/";
 const strTeamUrl = "https://projectv.gg/teams/";
@@ -169,12 +196,17 @@ async function getPlayerInfos(arrRiotIDs) {
     objPlayerInfo.RiotID = strRiotID;
     objPlayerInfo.Error = false;
     arrPromises.push(fetch(strApiUrl, {method: 'GET', mode: 'no-cors'})
-    .then(response => response.json())
-    .then(jsonData => {
-      console.log(jsonData);
-      objPlayerInfo.RankImg = jsonData.data.images.large;
-      objPlayerInfo.RankName = jsonData.data.currenttierpatched;
-      return objPlayerInfo;
+    .then(response => response.text())
+    .then(textData => {
+      let rankName = textData.match(/\[(.*?)\]/);
+      if (rankName == null) {
+        throw new Error("Rank not found");
+      } 
+      else {
+        objPlayerInfo.RankImg = textData.data.images.large;
+        objPlayerInfo.RankName = rankName;
+        return objPlayerInfo;
+      }
     })
     
     // fallback when playerInfo can't be fetched
